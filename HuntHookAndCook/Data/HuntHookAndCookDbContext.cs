d49efa11368498c2c4ee285d596d7e658c55c734
RickFiji <7011454+RickFiji@users.noreply.github.com>
2024-08-25 22:25:28 -0400

    Added recipe management pages

--- HuntHookAndCook/Data/HuntHookAndCookDbContext.cs
+++ HuntHookAndCook/Data/HuntHookAndCookDbContext.cs
@@ -1,4 +1,5 @@
-﻿using HuntHookAndCook.Models;
+﻿using HuntHookAndCook.Components.Pages.Recipe;
+using HuntHookAndCook.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace HuntHookAndCook.Data
@@ -7,5 +8,21 @@ namespace HuntHookAndCook.Data
     {
         public HuntHookAndCookDbContext(DbContextOptions<HuntHookAndCookDbContext> options) : base(options) { }
 
+        public DbSet<RecipeDefinition> Recipe { get; set; }
+        public DbSet<IngredientDefinition> Ingredient { get; set; }
+        public DbSet<StepDefinition> Step { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<RecipeDefinition>()
+                .HasMany(r => r.Ingredients)
+                .WithOne(i => i.Recipe)
+                .HasForeignKey(i => i.RecipeId);
+
+            modelBuilder.Entity<RecipeDefinition>()
+                .HasMany(r => r.Steps)
+                .WithOne(s => s.Recipe)
+                .HasForeignKey(s => s.RecipeId);
+        }
     }
 }
---
+++ HuntHookAndCook/Data/Seed/RecipeSeed.sql
@@ -0,0 +1,32 @@
+﻿--select * from dbo.Recipe
+--select * from dbo.Ingredient
+--select * from dbo.Step
+
+select 
+	r.Category,
+	r.Title as RecipeName,
+	i.Name as IngredientName,
+	i.Quantity,
+	i.Unit
+from dbo.Recipe r
+left join dbo.Ingredient i on r.Id = i.RecipeId
+
+select 
+	r.Category,
+	r.Title as RecipeName,
+	s.[Order],
+	s.Description
+from dbo.Recipe r
+left join dbo.Step s on r.Id = s.RecipeId
+
+--Insert into dbo.Recipe values ('Venison','Soup','Yummy',null)
+declare @recipeId int = (select top 1 Id from dbo.Recipe )
+--Insert into dbo.Ingredient values 
+--	('Venison',1,'lbs',@recipeId),
+--	('Water',4,'cups',@recipeId),
+--	('Salt',1,'tbspn',@recipeId)
+--Insert into dbo.Step values 
+--	(1,'Boil Water',@recipeId),
+--	(2,'Add meat',@recipeId),
+--	(3,'Wait',@recipeId),
+--	(4,'Eat',@recipeId)
