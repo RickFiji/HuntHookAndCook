6705711803a6476d72cafb51465b56e156311987
RickFiji <7011454+RickFiji@users.noreply.github.com>
2024-09-21 15:08:53 -0400

    Added Recipe card page

--- HuntHookAndCook/Data/HuntHookAndCookDbContext.cs
+++ HuntHookAndCook/Data/HuntHookAndCookDbContext.cs
@@ -6,15 +6,17 @@ namespace HuntHookAndCook.Data
 {
     public class HuntHookAndCookDbContext : DbContext
     {
-        public HuntHookAndCookDbContext(DbContextOptions<HuntHookAndCookDbContext> options) : base(options) { }
-
         public DbSet<RecipeDefinition> Recipe { get; set; }
         public DbSet<CategoryDefinition> Category { get; set; }
         public DbSet<IngredientDefinition> Ingredient { get; set; }
         public DbSet<StepDefinition> Step { get; set; }
 
+        public HuntHookAndCookDbContext(DbContextOptions<HuntHookAndCookDbContext> options) : base(options) { }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<RecipeDefinition>()
                 .HasMany(r => r.Ingredients)
                 .WithOne(i => i.Recipe)
